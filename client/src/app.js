d429e45fd581073da68e0ddb47bd540b95886b84
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-12 16:33:44 +0000

    refactored app.js

--- client/src/app.js
+++ client/src/app.js
@@ -2,54 +2,36 @@ var Bank = require('./bank/bank.js');
 var sampleAccounts = require('../sample.json');
 var Account = require('./bank/account.js');
 
-window.onload = function() {
-  console.log('loaded');
-  var bank = new Bank();
+var createItemForAccount = function(account) {
+  var accountListItem = document.createElement('li');
+  accountListItem.innerText = account.owner + ": £" + account.amount;
+  return accountListItem;
+};
 
+var populateAccountList = function(listElement, accounts) {
+  for(account of accounts) {
+    listElement.appendChild(createItemForAccount(account));
+  }
+};
+
+window.onload = function() {
+  var bank = new Bank();
   for (accountData of sampleAccounts){
     bank.addAccount(new Account(accountData));
   }
 
-  console.log("new bank", bank);
-  // TOTAL ------------------------------------------------------------------------
   var totalDisplay = document.getElementById('total');
-  totalDisplay.innerText = "Total: " + bank.totalCash();
-
-  var accountList = document.getElementById('accounts');
-  
-  for (account of bank.accounts){
-    var accountListItem = document.createElement('li');
-    accountListItem.innerText = account.owner + ": £" + account.amount.toFixed(2);
-    accountList.appendChild(accountListItem);
-  }
-
-  //BUSINESS ----------------------------------------------------------------------
   var businessTotalDisplay = document.getElementById('business-total');
-  businessTotalDisplay.innerText = "Business Total: " + bank.totalCash('business').toFixed(2);
-
-  var businessDisplay = document.getElementById('business-accounts');
-  
-  for (account of bank.accounts){
-    console.log(account.type);
-    if (account.type === 'business'){
-      var accountListItem = document.createElement('li');
-      accountListItem.innerText = account.owner + ": £" + account.amount.toFixed(2);
-      businessDisplay.appendChild(accountListItem);
-    }
-  }
-
-  //PERSONAL ----------------------------------------------------------------------
   var personalTotalDisplay = document.getElementById('personal-total');
+
+  totalDisplay.innerText = "Total: " + bank.totalCash();
+  businessTotalDisplay.innerText = "Business Total: " + bank.totalCash('business').toFixed(2);
   personalTotalDisplay.innerText = "personal Total: " + bank.totalCash('personal').toFixed(2);
 
+  var businessDisplay = document.getElementById('business-accounts');
   var personalDisplay = document.getElementById('personal-accounts');
 
-  for (account of bank.accounts){
-    if (account.type === 'personal'){
-    var accountListItem = document.createElement('li');
-    accountListItem.innerText = account.owner + ": £" + account.amount.toFixed(2);
-    personalDisplay.appendChild(accountListItem);
-    }
-  }
+  populateAccountList(businessDisplay, bank.filteredAccounts('business'));
+  populateAccountList(personalDisplay, bank.filteredAccounts('personal'));
 
 };
---
+++ client/views/bankView.js
@@ -0,0 +1,31 @@
+var Bank = require('./bank/bank.js');
+
+var bankView = function(){
+  var bank = new Bank();
+  this.render(bank);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = bankView;
