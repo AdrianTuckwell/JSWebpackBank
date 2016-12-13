8d325b7f98637ed3850b1bfa9e637cc8766ea5fb
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-13 07:34:54 +0000

    updated to limit cash to 2 decimal places when adding interest

--- client/views/bank_view.js
+++ client/views/bank_view.js
@@ -10,7 +10,7 @@ render: function(){
 
     var createItemForAccount = function(account) {
       var accountListItem = document.createElement('li');
-      accountListItem.innerText = account.owner + ": £" + account.amount;
+      accountListItem.innerText = account.owner + ": £" + account.amount.toFixed(2);
       return accountListItem;
     };
 
@@ -20,33 +20,33 @@ render: function(){
       }
     };
 
+    var resetDisplay = function(){
+      businessDisplay.innerHTML = "";
+      personalDisplay.innerHTML = "";
+    }
+
+
     var totalDisplay = document.getElementById('total');
     var businessTotalDisplay = document.getElementById('business-total');
     var personalTotalDisplay = document.getElementById('personal-total');
-
-    totalDisplay.innerText = "Total: " + this.bank.totalCash();
-    businessTotalDisplay.innerText = "Business Total: " + this.bank.totalCash('business').toFixed(2);
-    personalTotalDisplay.innerText = "personal Total: " + this.bank.totalCash('personal').toFixed(2);
-
     var businessDisplay = document.getElementById('business-accounts');
     var personalDisplay = document.getElementById('personal-accounts');
 
+    totalDisplay.innerText = "Total: " + this.bank.totalCash();
+    businessTotalDisplay.innerText = "Business Total: " + this.bank.totalCash('business').toFixed(2);
+    personalTotalDisplay.innerText = "personal Total: " + this.bank.totalCash('personal').toFixed(2);    
     populateAccountList(businessDisplay, this.bank.filteredAccounts('business'));
     populateAccountList(personalDisplay, this.bank.filteredAccounts('personal'));
 
     var button = document.getElementById('button');
     button.onclick = function() {
       
-      businessDisplay.innerHTML = "";
-      personalDisplay.innerHTML = "";
-
-      console.log('clicked button');
+      resetDisplay();
       this.bank.addInterest(1.1);
 
       totalDisplay.innerText = "Total: " + this.bank.totalCash().toFixed(2);
       businessTotalDisplay.innerText = "Business Total: " + this.bank.totalCash('business').toFixed(2);
       personalTotalDisplay.innerText = "personal Total: " + this.bank.totalCash('personal').toFixed(2);
-
       populateAccountList(businessDisplay, this.bank.filteredAccounts('business'));
       populateAccountList(personalDisplay, this.bank.filteredAccounts('personal'));
       }.bind(this);
